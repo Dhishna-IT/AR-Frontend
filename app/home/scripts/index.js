1cef86e8ca1aba576c528ac1304f07b35d8abcc1
AB7zz <abhinavcv007@gmail.com>
2023-11-13 14:05:10 +0530

    map and camera set up

--- app/home/scripts/index.js
+++ app/home/scripts/index.js
@@ -7,11 +7,21 @@ const mapScene = document.getElementById('mapScene')
 cameraBtn.addEventListener('click', () => {
     console.log('cameraBtn')
     cameraScene.style.display = 'block'
+    cameraScene.classList.add('block')
+    cameraScene.classList.remove('hidden')
+
     mapScene.style.display = 'none'
+    mapScene.classList.add('hidden')
+    mapScene.classList.remove('block')
 })
 
 mapBtn.addEventListener('click', () => {
     console.log('mapBtn')
     mapScene.style.display = 'block'
+    mapScene.classList.add('block')
+    mapScene.classList.remove('hidden')
+
     cameraScene.style.display = 'none'
+    cameraScene.classList.add('hidden')
+    cameraScene.classList.remove('block')
 })
