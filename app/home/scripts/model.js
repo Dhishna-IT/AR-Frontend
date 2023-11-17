ffb222282bb900a03177b6aa65a1e1fd7563ba37
SREERAG M <sreeragm030@gmail.com>
2023-11-17 15:51:45 +0530

    model array change ready

--- app/home/scripts/model.js
+++
@@ -1,5 +0,0 @@
-function updateModel(){
-    const gltfModel = document.getElementById('gltfModel')
-    const newModelURL = `3dAssets/${locIndex+1}.gltf`;
-    gltfModel.setAttribute('gltf-model', `${newModelURL}`);
-}
