0d55b4b8092cb57b4b51f581543cbb4d5f188a4a
i0dheerajdileep <i.dheerajdileep@gmail.com>
2023-11-19 17:52:45 +0530

    loc func

--- app/home/scripts/location.js
+++ app/home/scripts/location.js
@@ -1,40 +1,105 @@
-let loc;
+// Function to calculate distance between two points given their latitude and longitude
 let locIndex=0;
-let user;
-let x;
-let y;
-const uid = localStorage.getItem('uid')
 
-const locationPromise = fetch('https://ar-backend-7a3f65dd5c44.herokuapp.com/api/v1/location')
-.then(response => response.json())
-.then(data => {
-    loc = data.locations;
-    data.locations.forEach(item => {
-    console.log(item, "itemmmm");
-    });
-    console.log("location", loc);
-})
-.catch(error => console.error('Error fetching location:', error));
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371e3; // Radius of Earth in meters
+  const phi1 = lat1 * (Math.PI / 180);
+  const phi2 = lat2 * (Math.PI / 180);
+  const deltaPhi = (lat2 - lat1) * (Math.PI / 180);
+  const deltaLambda = (lon2 - lon1) * (Math.PI / 180);
 
-  // Fetch user data
-const userPromise = fetch(`https://ar-backend-7a3f65dd5c44.herokuapp.com/api/v1/user/${uid}`)
-.then(response => response.json())
-.then(data => {
-    user = data.result;
-    console.log(user, "user");
-})
-.catch(error => console.error('Error fetching user:', error));
+  const a = Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) +
+            Math.cos(phi1) * Math.cos(phi2) *
+            Math.sin(deltaLambda / 2) * Math.sin(deltaLambda / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-  // Wait for both promises to resolve
-Promise.all([locationPromise, userPromise])
-.then(() => {
-    // Now both fetch requests have completed
-    console.log("user level:", user.level);
-    if(user.level < locIndex +1)
-    {
-        x = loc[locIndex].x1
-        y = loc[locIndex].x2
-        console.log("x y", x, y)
-    }
-})
-.catch(error => console.error('Error:', error));
+  const distance = R * c;
+  return distance;
+}
+
+// Function to check if the user is near a location within a given threshold
+function isUserNearLocation(userLatt, userLong, locationLatt, locationLong, threshold) {
+  const distance = calculateDistance(userLatt, userLong, locationLatt, locationLong);
+  return distance <= threshold;
+}
+
+// Fetch user's real-time location using Geolocation API
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+      let userLatt = position.coords.latitude;
+      let userLong = position.coords.longitude;
+
+      // Fetch location data
+      const locationPromise = fetch('https://ar-backend-7a3f65dd5c44.herokuapp.com/api/v1/location')
+          .then(response => response.json());
+
+      // Fetch user data
+      const uid = localStorage.getItem('uid');
+      const userPromise = fetch(`https://ar-backend-7a3f65dd5c44.herokuapp.com/api/v1/user/${uid}`)
+          .then(response => response.json());
+
+      // Wait for both promises to resolve
+      Promise.all([locationPromise, userPromise])
+          .then(([locationData, userData]) => {
+              const loc = locationData.locations;
+              const user = userData.result;
+
+              // Function to continuously check user proximity
+              function checkProximity() {
+                  // Check if the user's level is below a certain threshold
+                  if (user.level < locIndex + 1) {
+                      const thresholdDistance = 10; // Set your desired threshold distance
+
+                      const isNearLocation = isUserNearLocation(
+                          userLatt,
+                          userLong,
+                          loc[locIndex].x1,
+                          loc[locIndex].x2,
+                          thresholdDistance
+                      );
+
+                      if (isNearLocation) {
+                          // Code to make the model visible
+                          const newLatitude = loc[locIndex] ? String(loc[locIndex].x1) : '10.046942288501658';
+                          const newLongitude = loc[locIndex] ? String(loc[locIndex].x2) : '76.33519972545754';
+                          const modelElement = document.getElementById('gltfModel');
+
+                          if (modelElement) {
+                              modelElement.setAttribute('gps-new-entity-place', `latitude: ${newLatitude}; longitude: ${newLongitude};`);
+                            }
+                            console.log("User is near the location ");
+
+                      } else {
+                          console.log("User is not near the location.");
+                      }
+                  } else {
+                      console.log("User level is not below the required threshold.");
+                  }
+
+                  // Fetch the user's location again and continue checking
+                  navigator.geolocation.getCurrentPosition(
+                      (position) => {
+                          userLatt = position.coords.latitude;
+                          userLong = position.coords.longitude;
+                          checkProximity();
+                      },
+                      (error) => {
+                          console.error('Error getting user location:', error);
+
+                          // Retry after an error with a short delay (adjust as needed)
+                          setTimeout(() => {
+                              checkProximity();
+                          }, 1000); // Retry after 1 second
+                      }
+                  );
+              }
+
+              // Start the continuous proximity check
+              checkProximity();
+          })
+          .catch(error => console.error('Error fetching data:', error));
+  },
+  (error) => {
+      console.error('Error getting user location:', error);
+  }
+);
--- app/home/scripts/model.js
+++ app/home/scripts/model.js
@@ -1,5 +1,48 @@
-function updateModel(){
-    const gltfModel = document.getElementById('gltfModel')
-    const newModelURL = `3dAssets/${locIndex+1}.gltf`;
-    gltfModel.setAttribute('gltf-model', `${newModelURL}`);
-}
+
+AFRAME.registerComponent('mycomponent', {
+    init: function () {
+      this.el.addEventListener('click', this.handleTouchStart.bind(this));
+      this.el.addEventListener('touchstart', this.handleTouchStart.bind(this));
+    },
+
+    handleTouchStart: function () {
+      this.stopCamera();
+    },
+
+    stopCamera: function () {
+      const sceneEl = this.el.sceneEl;
+      const camera = sceneEl.camera;
+
+      // Check if the camera has a reference to the stream
+      if (camera && camera.components['device-orientation']) {
+        const cameraStream = camera.components['device-orientation'].sensorSource.sourceStream;
+
+        // Check if the cameraStream is available
+        if (cameraStream) {
+          const tracks = cameraStream.getTracks();
+          tracks.forEach(track => track.stop());
+          console.log('Camera stopped.');
+        } else {
+          console.warn('Camera stream not available.');
+        }
+      } else {
+        console.warn('Camera component not found.');
+      }
+    }
+  });
+
+  function updateModel() {
+    console.log("updating model called");
+    var model = document.getElementById("gltfModel");
+    model.removeAttribute("gltf-model");
+    const newModelURL = `3dAssets/${locIndex + 1}.glb`
+    locIndex=locIndex + 1;
+    console.log(`${locIndex + 1}`);
+    model.setAttribute('gltf-model', `${newModelURL}`);
+    console.log("model updated");
+  }
+
+  const mybutton2 = document.getElementById("mapBtn");
+  mybutton2.addEventListener("click", () => {
+    updateModel();
+  });
